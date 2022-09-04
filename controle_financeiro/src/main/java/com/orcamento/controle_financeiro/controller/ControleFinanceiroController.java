c715032a9f50a29c87bf1c0cb56d95de4a00c5d9
SannyHL <sannyhelenlima@gmail.com>
2022-09-04 13:59:51 -0300

    Criação Métodos (Get, Post, Put e Delete).

--- controle_financeiro/src/main/java/com/orcamento/controle_financeiro/controller/ControleFinanceiroController.java
+++ controle_financeiro/src/main/java/com/orcamento/controle_financeiro/controller/ControleFinanceiroController.java
@@ -1,7 +1,13 @@
 package com.orcamento.controle_financeiro.controller;
 
+import com.orcamento.controle_financeiro.service.ControleFinanceiroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ControleFinanceiroController {
+
+    @Autowired
+    public ControleFinanceiroService controleFinanceiroService;
+
 }
--- controle_financeiro/src/main/java/com/orcamento/controle_financeiro/controller/GrupoPessoasController.java
+++ controle_financeiro/src/main/java/com/orcamento/controle_financeiro/controller/GrupoPessoasController.java
@@ -1,7 +1,80 @@
 package com.orcamento.controle_financeiro.controller;
 
+import com.orcamento.controle_financeiro.dto.GrupoPessoasDto;
+import com.orcamento.controle_financeiro.model.GrupoPessoas;
+import com.orcamento.controle_financeiro.service.GrupoPessoasService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/pessoas")
 public class GrupoPessoasController {
+
+    @Autowired
+    public GrupoPessoasService grupoPessoasService;
+
+    @GetMapping("/cadastro")
+    public String getAllNavegador(){
+        return "pessoas";
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<GrupoPessoas> createPessoaNavegador(@Valid GrupoPessoasDto grupoPessoasDto){
+        var grupoPessoas = new GrupoPessoas();
+        BeanUtils.copyProperties(grupoPessoasDto, grupoPessoas);
+        return new ResponseEntity<GrupoPessoas>(grupoPessoasService.create(grupoPessoas), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<GrupoPessoas> getAll(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<GrupoPessoas> createPessoa(@RequestBody @Valid GrupoPessoasDto grupoPessoasDto){
+        var grupoPessoas = new GrupoPessoas();
+        BeanUtils.copyProperties(grupoPessoasDto, grupoPessoas);
+        return new ResponseEntity<GrupoPessoas>(grupoPessoasService.create(grupoPessoas), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GrupoPessoas> deletePessoa(@PathVariable(value = "id") Integer id){
+        Optional<GrupoPessoas> grupoPessoasOptional = grupoPessoasService.findId(id);
+        if (grupoPessoasOptional.isPresent()){
+            grupoPessoasService.delete(grupoPessoasOptional.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GrupoPessoas> getId(@PathVariable(value = "id") Integer id){
+        Optional<GrupoPessoas> grupoPessoasOptional = grupoPessoasService.findId(id);
+        if (grupoPessoasOptional.isPresent()){
+            return new ResponseEntity<>(grupoPessoasOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GrupoPessoas> putPessoas(@PathVariable(value = "id") Integer id, @RequestBody @Valid GrupoPessoasDto grupoPessoasDto){
+        Optional<GrupoPessoas> grupoPessoasOptional = grupoPessoasService.findId(id);
+        if (grupoPessoasOptional.isPresent()){
+            var grupoPessoas = new GrupoPessoas();
+            BeanUtils.copyProperties(grupoPessoasDto, grupoPessoas);
+            return new ResponseEntity<GrupoPessoas>(grupoPessoasService.create(grupoPessoas), HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }

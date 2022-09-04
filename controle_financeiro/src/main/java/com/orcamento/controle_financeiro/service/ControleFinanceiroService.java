1f81db26842714333dc262e015a7a89255b0fe04
SannyHL <sannyhelenlima@gmail.com>
2022-09-04 14:38:40 -0300

    Criação Métodos.

--- controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/ControleFinanceiroService.java
+++ controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/ControleFinanceiroService.java
@@ -1,7 +1,14 @@
 package com.orcamento.controle_financeiro.service;
 
+import com.orcamento.controle_financeiro.repository.ControleFinanceiroRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ControleFinanceiroService {
+
+    @Autowired
+    public ControleFinanceiroRepo controleFinanceiroRepo;
+
+
 }
--- controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/GrupoPessoasService.java
+++ controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/GrupoPessoasService.java
@@ -1,7 +1,29 @@
 package com.orcamento.controle_financeiro.service;
 
+import com.orcamento.controle_financeiro.model.GrupoPessoas;
+import com.orcamento.controle_financeiro.repository.GrupoPessoasRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GrupoPessoasService {
+
+    @Autowired
+    public GrupoPessoasRepo grupoPessoasRepo;
+
+
+    public GrupoPessoas create(GrupoPessoas grupoPessoas) {
+        return grupoPessoasRepo.save(grupoPessoas);
+    }
+
+    public Optional<GrupoPessoas> findId(Integer id) {
+        return grupoPessoasRepo.findById(id);
+    }
+
+
+    public void delete(GrupoPessoas grupoPessoas) {
+        grupoPessoasRepo.delete(grupoPessoas);
+    }
 }

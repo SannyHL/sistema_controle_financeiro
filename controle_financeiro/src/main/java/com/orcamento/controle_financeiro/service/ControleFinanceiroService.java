60b725d2f887ba8474818883301329558696f70c
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-05 14:05:10 -0300

    Criação de métodos.

--- controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/ControleFinanceiroService.java
+++ controle_financeiro/src/main/java/com/orcamento/controle_financeiro/service/ControleFinanceiroService.java
@@ -1,8 +1,12 @@
 package com.orcamento.controle_financeiro.service;
 
+import com.orcamento.controle_financeiro.model.ControleFinanceiro;
 import com.orcamento.controle_financeiro.repository.ControleFinanceiroRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class ControleFinanceiroService {
@@ -11,4 +15,15 @@ public class ControleFinanceiroService {
     public ControleFinanceiroRepo controleFinanceiroRepo;
 
 
+    public ControleFinanceiro create(ControleFinanceiro controleFinanceiro) {
+        return controleFinanceiroRepo.save(controleFinanceiro);
+    }
+
+    public Optional<ControleFinanceiro> findId(Date dataGasto) {
+        return controleFinanceiroRepo.findById(dataGasto);
+    }
+
+    public void delete(ControleFinanceiro controleFinanceiro) {
+        controleFinanceiroRepo.delete(controleFinanceiro);
+    }
 }
